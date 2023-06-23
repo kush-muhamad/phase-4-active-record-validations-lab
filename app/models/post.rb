0891aaf5d0db27db65eb0220d2ce16dd0379d421
kush Muhamad <kushmohammed98@yahoo.com>
2023-06-23 21:30:18 +0300

    completed task

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait_title
+  
+    private
+  
+    def clickbait_title
+      unless title&.include?("Won't Believe") || title&.include?('Secret') || title&.match?(/\bTop \d+\b/) || title&.include?('Guess')
+        errors.add(:title, "must be sufficiently clickbait-y")
+      end
+    end
+  end
+  
